44ee2bcb9aeee09454487baba0b26cdcbe7ecd6a
Bostonhome <liangyunbo@yahoo.com>
2023-03-17 22:02:37 +0800

    make first analysis and plot to git

--- section1_03172023.R
+++ section1_03172023.R
@@ -1,5 +1,14 @@
 library(tidyverse)
+library(dslabs)
+data(murders)
+library(ggplot2)
 
 # This is first r file of first repository in git
 # To make sync for the first time
 
+head(murders)
+tail(murders)
+murders_rate <- murders %>% mutate(rate = total/population*100000) %>%
+  ggplot(aes(state, rate)) + 
+  geom_point()
+murders_rate  
